ec5680798e0871f823989b79fbcf8589a3c29b33
fudgemasterultra <79956447+fudgemasterultra@users.noreply.github.com>
2022-10-15 11:57:15 -0400

    added sql  connection
    added sql connection to db

--- sqlcommands/sql.go
+++ sqlcommands/sql.go
@@ -1,10 +1,28 @@
 package sqllayer
 
 import (
+	"database/sql"
 	"fmt"
+
+	_ "github.com/lib/pq"
 )
 
+const (
+	host	 = "localhost"
+	port	 = 5432
+	user	 = "postgres"
+	sbCustomerDb = "SB_Customers"
+)
 
 func ConnectToDB(password  string){
-	fmt.Println("password is: ", password)
+	sqlConnectionString := fmt.Sprintf("host=%s port=%d user=%s password=%s dbname=%s sslmode=disable", host, port, user, password, sbCustomerDb)
+	db, err := sql.Open("postgres", sqlConnectionString)
+	if err != nil {
+		panic(err)
+	}
+	defer db.Close()
+	err = db.Ping()
+	if err != nil {
+		panic(err)
+	}
 }
